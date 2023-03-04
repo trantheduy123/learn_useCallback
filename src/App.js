3e010a28a8ace954fd29b96efe4ce2a7a233a59b
The Duy <trantheduy2309@gmail.com>
2023-03-04 14:41:46 +0700

    finish use memo

--- src/App.js
+++ src/App.js
@@ -1,29 +1,51 @@
 import React, { useCallback, useState } from "react";
-import Childcomponent from "./component/Childcomponent";
+import Child from "./component/Child";
 
-function App() {
+function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <p> Count : {count} </p>
+      <button onClick={() => setCount(count + 1)}>Add</button>
+    </>
+  );
+}
+
+function App({ ViewsCount }) {
+  const [name, setName] = useState("Duy");
+
+  const getUser = useCallback(() => {
+    return fetch(`https://reqres.in/api/users`);
+  }, []);
+
+  /* 
+  use call back
   const [users, setUsers] = useState([]);
 
   const getData = useCallback((type) => {
     return fetch(`https://reqres.in/api/${type}`);
   }, []);
 
-  const handleClick = () => {
+ 
+   
+    const handleClick = () => {
     getData("users")
       .then((res) => res.json())
       .then((res) => {
         const users = res.data;
         setUsers(users);
-      });
-  };
+      });  */
 
   return (
     <>
-      <p>Data :</p>
+      <Counter />
+      <Child name={name} getUser={getUser} />
+    </>
+    /* use Callback
+       <p>Data :</p>
       <button onClick={handleClick}>Get user data</button>
       <p>{JSON.stringify(users)}</p>
-      <Childcomponent getData={getData} />
-    </>
+      <Childcomponent getData={getData} /> */
   );
 }
 
---
+++ src/component/Child.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Child() {
+  console.log("Child component - render");
+  return (
+    <div>
+      <p>Child component</p>
+    </div>
+  );
+}
+
+export default Child;
